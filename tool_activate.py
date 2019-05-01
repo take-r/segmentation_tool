089f9e2bb4d361e9c3d60f80926e3b7bb61b880f
大竹亮馬 <take@ootakeryoubanoMacBook.local>
2019-05-01 16:41:49 +0900

    select directory by GUI

--- tool_activate.py
+++ tool_activate.py
@@ -1,9 +1,12 @@
 import tkinter
-import dir_choice
+import dir_select
 
 #イベント設定
 def dir_mode():
-    dirchoice.choice_mode()
+    global img
+    file_path = dirselect.select_mode()
+    img = tkinter.PhotoImage(file = file_path)
+    canvas.create_image(310, 200, image = img)
 
 
 # ウインドウを作る
@@ -16,15 +19,17 @@ root.option_add("*font", ["MS Pゴシック", 22])
 canvas = tkinter.Canvas(bg = "black", width = 640, height = 480)
 canvas.place(x=0, y=0)
 canvas.create_rectangle(0, 0, 600, 300, fill="gray")
+img = tkinter.PhotoImage(file = "chap4-1-1.png") #初期画像設定　この行だけだと画像は非表示
 
-#画像選択ボタン表示
-dirReset_button = tkinter.Button(text = "画像選択")
-dirReset_button.place(x=410, y=133)
-dirReset_button["command"] = dir_mode
+#ファイル選択ボタン表示
+dirSelect_button = tkinter.Button(text = "ファイル選択")
+dirSelect_button.place(x=410, y=133)
+dirSelect_button["command"] = dir_mode
+
+
 
 #オブジェクト設定
-dirchoice = dir_choice.DirChoice()
-
+dirselect = dir_select.DirSelect()
 
 # ウインドウを表示する
 root.mainloop()
